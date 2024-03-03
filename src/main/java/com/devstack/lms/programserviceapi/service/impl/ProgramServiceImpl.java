0eaf3c1c51a1c46b4930d80f11b8b33b04beaf62
ozen-hub <info.ozenhub@gmail.com>
2024-03-03 21:14:33 +0530

    4

--- src/main/java/com/devstack/lms/programserviceapi/service/impl/ProgramServiceImpl.java
+++ src/main/java/com/devstack/lms/programserviceapi/service/impl/ProgramServiceImpl.java
@@ -2,16 +2,21 @@ package com.devstack.lms.programserviceapi.service.impl;
 
 import com.devstack.lms.programserviceapi.dto.request.RequestProgramDto;
 import com.devstack.lms.programserviceapi.entity.Program;
+import com.devstack.lms.programserviceapi.entity.Subject;
 import com.devstack.lms.programserviceapi.repo.ProgramRepository;
 import com.devstack.lms.programserviceapi.service.ProgramService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProgramServiceImpl implements ProgramService {
 
     private final ProgramRepository programRepository;
+    private final WebClient webClient;
 
     @Override
     public void createProgram(RequestProgramDto programDto) {
@@ -22,8 +27,22 @@ public class ProgramServiceImpl implements ProgramService {
                 .subjects(programDto.getSubjects())
                 .build();
 
-        // get All subjects and need to check if there are available or not
+        List<Long> ids = program.getSubjects().stream()
+                        .map(Subject::getId).toList();
 
-        programRepository.save(program);
+        System.out.println(ids);
+
+        Boolean isOk = webClient.get().uri("http://localhost:8082/api/v1/subjects",
+                uriBuilder -> uriBuilder.pathSegment(String.valueOf(ids)).build())
+                        .retrieve()
+                                .bodyToMono(Boolean.class)
+                                        .block();
+
+        if(Boolean.TRUE.equals(isOk)){
+            programRepository.save(program);
+        }else{
+            throw new IllegalArgumentException("Try again with available Languages");
+        }
+
     }
 }
