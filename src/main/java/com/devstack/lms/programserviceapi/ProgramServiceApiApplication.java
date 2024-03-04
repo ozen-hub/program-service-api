e605cd401273649c6314f980a350e582eff37919
ozen-hub <info.ozenhub@gmail.com>
2024-03-04 21:08:20 +0530

    7

--- src/main/java/com/devstack/lms/programserviceapi/ProgramServiceApiApplication.java
+++ src/main/java/com/devstack/lms/programserviceapi/ProgramServiceApiApplication.java
@@ -2,9 +2,10 @@ package com.devstack.lms.programserviceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurkaClient
+@EnableDiscoveryClient
 public class ProgramServiceApiApplication {
 
 	public static void main(String[] args) {
